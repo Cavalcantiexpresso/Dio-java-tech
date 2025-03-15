512703de7154d3899ff7804f6b087282c28e7e5a
Luciano Paiva Cavalcanti <lucianocavalcantipf@icloud.com>
2025-03-15 02:45:15 -0300

    atualizando o mapper

---
+++ src/main/java/br/mapper/UserMapper.java
@@ -0,0 +1,22 @@
+package br.mapper;
+
+
+import br.dto.UserDTO;
+import br.model.UserModel;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper
+public interface UserMapper {
+
+    @Mapping(target = "code", source = "id")
+    @Mapping(target = "userName", source = "name")
+    UserModel toModel(final UserDTO dto);
+
+
+
+    @Mapping(target = "id", source = "code")
+    @Mapping(target = "name", source = "userName")
+    UserDTO toDTO(final UserModel model);
+
+}
